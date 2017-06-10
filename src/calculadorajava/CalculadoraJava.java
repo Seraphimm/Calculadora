532e0045b55ec85631934bd0b53336d776c2e326
Aluno <Aluno@lab01-21>
2017-06-10 17:17:55 -0300

    git2

--- src/calculadorajava/CalculadoraJava.java
+++ src/calculadorajava/CalculadoraJava.java
@@ -5,6 +5,8 @@
  */
 package calculadorajava;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Jefferson
@@ -15,7 +17,17 @@ public class CalculadoraJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        float a, b;
+        a = Float.parseFloat(JOptionPane.showInputDialog(null, "insira o primeiro número: "));
+        b = Float.parseFloat(JOptionPane.showInputDialog(null, "insira o segundo número: "));
+        
+        Multiplcaçao m = new Multiplcaçao();
+        System.out.println(m.mut(a, b));
+        
+        
+        Subtraçao s = new Subtraçao();
+        System.out.println(s.sub(a, b));
+         
     }
     
 }
