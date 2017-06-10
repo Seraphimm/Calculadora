3faaebbd8c2ce8fc0c20678d8b917abad9d81af7
Aluno <Aluno@lab01-21>
2017-06-10 17:18:11 -0300

    git2

---
+++ src/calculadorajava/Multiplcaçao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadorajava;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Multiplcaçao {
+    float mut(float a, float b){
+        return a*b;
+    }
+}
